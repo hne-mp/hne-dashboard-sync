ae9adf08053b4593fecc1f098841ac54078bb530
jackson <jacksond2789@gmail.com>
2022-01-16 12:54:40 +0700

    disable log

--- src/DBConnect.ts
+++ src/DBConnect.ts
@@ -1,12 +1,12 @@
 import { Sequelize } from "sequelize";
 import config from "./config";
-import { logger } from "./utils/logger";
+// import { logger } from "./utils/logger";
 const connection = new Sequelize(config.connectionString, {
   // Choose one of the logging options
   // logging: console.log, // Default, displays the first parameter of the log function call
   // logging: (...msg) => console.log(msg), // Displays all log function call parameters
   // logging: false, // Disables logging
-  logging: (msg) => logger.debug(msg), // Use custom logger (e.g. Winston or Bunyan), displays the first parameter
+  // logging: (msg) => logger.debug(msg), // Use custom logger (e.g. Winston or Bunyan), displays the first parameter
   // logging: logger.debug.bind(logger), // Alternative way to use custom logger, displays all messages
 });
 
